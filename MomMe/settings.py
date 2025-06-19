2fae8c1ab142475ad5e6c853c9a3b954b170fc6e
Kier Roman <kierroman@gmail.com>
2025-06-19 01:20:32 -0500

    Enable debug mode for error diagnostics

--- MomMe/settings.py
+++ MomMe/settings.py
@@ -139,7 +139,7 @@ STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 LOGIN_REDIRECT_URL = '/dashboard/'
 
